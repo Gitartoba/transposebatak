aa0c1a312eeb2796d7fbeb6fe7000672ddf87fbd
Gitartoba <64688559+Gitartoba@users.noreply.github.com>
2020-05-02 19:18:01 +0700

    Create transposer.css

---
+++ transposer.css
@@ -0,0 +1,29 @@
+.transpose-keys {
+	margin: 10px 0;
+	overflow: auto;
+	font: normal 14px Roboto;
+	padding-top: 10px;
+	clear: both;
+}
+.transpose-keys a { 
+	display: block;
+	float: left;
+	width: 2.25em;
+	text-align: center;
+	margin: 0 .25em .25em 0;
+	color: #333;
+	background: #eee;
+	text-decoration: none;
+	padding: .4em 0;
+	border: solid 1px black;
+	outline: none;
+}
+.transpose-keys a.selected { 
+  background: #ed1c24; 
+  color: #FFF;
+}
+.entry-content .c,.entry-content .on{
+  color: #1923d6;
+  transition: opacity 0.3s ease-in-out;
+  
+}
